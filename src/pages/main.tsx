a08fe9f4a7fe399d509dc4ea00bb0a4b300d1c94
json91-dev <jjjjjw910911@gmail.com>
2024-04-30 16:17:02 +0900

    [feat] 템플릿 설정

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import MainLayout from "@src/layouts/MainLayout.tsx";
+import React from 'react'
+import MainLayout from '@src/layouts/MainLayout.tsx'
 export default function MainPage() {
-  return (
-    <div className={'border-2 text-[2rem] flex justify-center'}>기본 페이지 작업</div>
-  )
-
-  
-
-
+  return <div className={'flex justify-center border-2 text-[2rem]'}>기본 페이지 작업</div>
 }
