068c1d5b734ebbc89b7fc31d360aeee17d7f831d
json91-dev <jjjjjw910911@gmail.com>
2024-11-18 14:41:02 +0900

    [feat] tailwind 설정 업데이트 및 일부 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Route, Routes, Navigate } from 'react-router-dom'
 import { lazy } from 'react'
-import MainLayout from '@src/layouts/MainLayout.tsx'
 const MainPage = lazy(() => import('@src/pages/main.tsx'))
 const ImagePage = lazy(() => import('@src/pages/image.tsx'))
 
 export default function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<MainPage />} />
-      </Route>
+      <Route index element={<MainPage />} />
       <Route path={'/image'} element={<ImagePage />} />
       <Route path='/*' element={<Navigate to={'/'} />} />
     </Routes>
